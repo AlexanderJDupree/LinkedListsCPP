ae407ddb49d1a870e60444c52bd44b23b6e62e55
Alexander DuPree <alexander.j.dupree@gmail.com>
2018-05-31 23:30:46 -0700

    Created initial framework for LinkedList class

---
+++ src/linkedList.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+* File: linkedList.h
+*
+* Description: 
+* 
+* Compiler: GNU GCC 5.4.0
+*
+* Date: 2018 - 05 - 31
+*******************************************************************************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+class LinkedList
+{
+public:
+    LinkedList() {}
+
+private:
+
+};
+
+
+#endif // LINKED_LIST_H
