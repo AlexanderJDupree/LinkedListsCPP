0b736149154ea92227c7493352919147b1260a33
Alexander DuPree <alexander.j.dupree@gmail.com>
2018-05-31 23:12:10 -0700

    Added third-party Catch2 framework

---
+++ tests/linkedListTest.cpp
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* File: linkedListTest.cpp
+*
+* Description: Driver runs all unit tests for our linkedList class 
+*
+* Author: Alexander DuPree
+* 
+* Compiler: GNU GCC 5.4.0
+*
+*******************************************************************************/
+
+#include "catch.hpp"
+
+TEST_CASE("Build and Run", "[build], [runtime]")
+{
+    REQUIRE(0 == 0);
+}
---
+++ tests/tests_main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* File: tests_main.cpp
+*
+* Description: Tests main is the entry point for our unit tests application. 
+               To reduce compilation time, compile this file first, then link 
+               its object file onto the unit tests source file. 
+*
+* Author: Alexander DuPree
+* 
+* Compiler: GNU GCC 5.4.0
+*
+* Date: 2018 - 05 - 31
+*******************************************************************************/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
