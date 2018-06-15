5845576f9d1eebe0c1732c1bbc19ded384f9b80b
Alex DuPree <alexander.j.dupree@gmail.com>
2018-06-15 16:59:58 -0700

    Basic linting for code style issues

--- tests/linkedListTest.cpp
+++ tests/linkedListTest.cpp
@@ -23,23 +23,23 @@ TEST_CASE("Constructing Nodes", "[node], [constructors]")
     {
         Node<int> node;
 
-        REQUIRE(*node.Data() == int());
-        REQUIRE(node.Next() == nullptr);
+        REQUIRE(*node.data() == int());
+        REQUIRE(node.next() == nullptr);
     }
     SECTION("Overloaded Construction")
     {
         Node<char> node('n');
 
-        REQUIRE(*node.Data() == 'n');
-        REQUIRE(node.Next() == nullptr);
+        REQUIRE(*node.data() == 'n');
+        REQUIRE(node.next() == nullptr);
     }
     SECTION("Copy construction")
     {
         Node<int> node1(5);
         Node<int> node2(node1);
 
-        REQUIRE(*node2.Data() == 5);
-        REQUIRE(node2.Next() == nullptr);
+        REQUIRE(*node2.data() == 5);
+        REQUIRE(node2.next() == nullptr);
     }
 }
 
@@ -51,22 +51,22 @@ TEST_CASE("Using Mutators to manipulate members", "[node], [mutators]")
         Node<char>  node2;
         Node<int>   node3;
 
-        node1.Data(3.545);
-        node2.Data('A');
-        node3.Data(77);
+        node1.data(3.545);
+        node2.data('A');
+        node3.data(77);
 
-        REQUIRE(*node1.Data() == float(3.545));
-        REQUIRE(*node2.Data() == 'A');
-        REQUIRE(*node3.Data() == 77);
+        REQUIRE(*node1.data() == float(3.545));
+        REQUIRE(*node2.data() == 'A');
+        REQUIRE(*node3.data() == 77);
     }
     SECTION("Manipulate next Node member")
     {
         Node<int> node1;
         Node<int> node2;
 
-        node1.Next(&node2);
+        node1.next(&node2);
 
-        REQUIRE(node1.Next() == &node2);
+        REQUIRE(node1.next() == &node2);
     }
 }
 
@@ -77,9 +77,9 @@ TEST_CASE("Link Nodes and access linked data members", "[node], [linked]")
         Node<int> node1(1);
         Node<int> node2(7);
 
-        node1.Next(&node2);
+        node1.next(&node2);
 
-        REQUIRE(*node1.Next()->Data() == 7);
+        REQUIRE(*node1.next()->data() == 7);
     }
 }
 
